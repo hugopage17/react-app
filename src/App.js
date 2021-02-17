bb03e63500cd56beb4dbfd8e18ec3d8c029e88e0
hugopage0017@gmail.com <>
2021-02-18 12:06:58 +1300

    'cache'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React and S3 app and Lambda too
+          React app template
         </p>
         <a
           className="App-link"
