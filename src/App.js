b1c64a2caf3d0ca95d309f3e856e26f82cb4349e
hugopage0017@gmail.com <>
2021-02-18 11:27:00 +1300

    'new'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React and S3 app
+          React and S3 app and Lambda
         </p>
         <a
           className="App-link"
